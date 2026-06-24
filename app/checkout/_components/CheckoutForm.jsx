4d21f2275bfdd3033b270382e2734c8f4a888c33
integramagna <hi@integramagna.com>
2026-06-24 13:40:33 +0530

    cluth updateseveegegvev

--- app/checkout/_components/CheckoutForm.jsx
+++ app/checkout/_components/CheckoutForm.jsx
@@ -525,6 +525,7 @@ export default function CheckoutForm({
 
             <div className={styles.ExpressContainer}>
               <ExpressCheckoutElement
+                paymentMethods={{ applePay: "always", googlePay: "always" }}
                 onConfirm={handleExpressCheckoutConfirm}
                 onClick={({ resolve }) =>
                   resolve({ emailRequired: true, phoneNumberRequired: true })
